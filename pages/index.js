4ee64fa332dba69d04b0a1595417e1eb1e2bcb14
jungseokman <seokman.jung@gmail.com>
2022-02-28 22:23:19 +0900

    second

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,79 @@
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
-const API_KEY = "10923b261ba94d897ac6b81148314a3f";
-
-export default function Home() {
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        (async() => {
-            const {results} = await (
-                await fetch(
-                    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-                )
-            ).json();
-            setMovies(results);
-        })();
-    }, [])
-    console.log(movies)
-    return (
-        <div>
-            <Seo title="Home" />
-            {movies.map((movie) => {
-                return (
-                    <div key={movie.id}>
-                        <h4>{movie.original_title}</h4>
-                    </div>
-                )
-            })}
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
+  return (
+    <div className="container">
+      <Seo title="Home" />
+      {results?.map((movie) => (
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+            >
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
-    )
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+}
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
